eafc106e9c6df4ac9ff9b19acb1eda7b8c9085e1
Robert Heler <robertheler@gmail.com>
2020-04-20 16:14:50 -0700

    refactor

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,30 +1,55 @@
+// model
+let game = [];
+let winner = 'X';
+//let tally = {X: 0, O: 0};
+let player = 'X';
+let playerOne = {score: 0, name: 'X'};
+let playerTwo = {score: 0, name: 'O'};
+
+//view
 const turn = document.getElementById('turn');
 const score = document.getElementById('score');
-let game = [];
-let player;
-let winner = 'X';
-let tally = {X: 0, O: 0}
 
-function clickCellHandler(event) {
-  let cell = event.target;
-  if (game[cell.id] === 'not played') {
-    playCell(cell);
-    togglePlayer();
-    checkForVictory();
+//controller
+resetBoard();
+document.querySelectorAll('td').forEach(tableCell => {
+  tableCell.addEventListener("click", (event) => {
+    let cell = event.target;
+    if (game[cell.id] === 'not played') {
+      playCell(cell);
+      togglePlayer();
+      checkForVictory();
+    }
+  });
+});
+
+document.getElementById('reset').addEventListener("click", resetBoard);
+
+function resetBoard (event) {
+  for (var i = 1; i <= 9; i++) {
+    game[i] = 'not played';
+    document.getElementById(i).innerHTML = '';
+    document.getElementById(i).style.color = 'gray';
   }
+
+  player = winner;
+  turn.style.color = 'gray'
+  turn.innerHTML = `${playerOne.name}, it's your turn!`
+  score.innerHTML = `${playerOne.name} ${playerOne.score}:${playerTwo.score} ${playerTwo.name}`
 }
 
-let togglePlayer = () => {
+
+togglePlayer = () => {
   if (player === 'X') {
     player = 'O';
-    turn.innerHTML = `Player O, it's your turn!`
+    turn.innerHTML = `${playerTwo.name}, it's your turn!`
   } else {
     player = 'X';
-    turn.innerHTML = `Player X, it's your turn!`
+    turn.innerHTML = `${playerOne.name}, it's your turn!`
   }
 }
 
-let playCell = (cell) => {
+playCell = (cell) => {
   if (player === 'O') {
     game[cell.id] = 'O';
     cell.style.color = 'black';
@@ -33,13 +58,13 @@ let playCell = (cell) => {
   }
   cell.innerHTML = player;
 }
-let checkForVictory = () => {
+checkForVictory = () => {
   checkRows();
   checkColumns();
   checkDiagonals();
 }
 
-let checkRows = () => {
+checkRows = () => {
   if (game[1] + game[2] + game[3] === 'XXX') declareVictory('X', 1, 2, 3);
   if (game[4] + game[5] + game[6] === 'XXX') declareVictory('X', 4, 5, 6);
   if (game[7] + game[8] + game[9] === 'XXX') declareVictory('X', 7, 8, 9);
@@ -48,7 +73,7 @@ let checkRows = () => {
   if (game[7] + game[8] + game[9] === 'OOO') declareVictory('O', 7, 8, 9);
 }
 
-let checkColumns = () => {
+checkColumns = () => {
   if (game[1] + game[4] + game[7] === 'XXX') declareVictory('X', 1, 4, 7);
   if (game[2] + game[5] + game[8] === 'XXX') declareVictory('X', 2, 5, 8);
   if (game[3] + game[6] + game[9] === 'XXX') declareVictory('X', 3, 6, 9);
@@ -57,17 +82,17 @@ let checkColumns = () => {
   if (game[3] + game[6] + game[9] === 'OOO') declareVictory('O', 3, 6, 9);
 }
 
-let checkDiagonals = () => {
+checkDiagonals = () => {
   if (game[1] + game[5] + game[9] === 'XXX') declareVictory('X', 1, 5, 9);
   if (game[3] + game[5] + game[7] === 'XXX') declareVictory('X', 3, 5, 7);
   if (game[1] + game[5] + game[9] === 'OOO') declareVictory('O', 1, 5, 9);
   if (game[3] + game[5] + game[7] === 'OOO') declareVictory('O', 3, 5, 7);
 }
 
-let declareVictory = (player, cell1, cell2, cell3) => {
+declareVictory = (player, cell1, cell2, cell3) => {
   turn.style.color = 'red'
-  turn.innerHTML = `Player ${player} won!`
 
+  turn.innerHTML = player === 'X' ? `${playerOne.name} won!` : `${playerTwo.name} won!`
 
   document.getElementById(cell1).style.color = 'red';
   document.getElementById(cell2).style.color = 'red';
@@ -79,25 +104,7 @@ let declareVictory = (player, cell1, cell2, cell3) => {
   }
 
   winner = player;
-  tally[winner]++;
+  winner === 'X' ? playerOne.score++ : playerTwo.score++;
 }
 
-document.querySelectorAll('td').forEach(tableCell => {
-  tableCell.addEventListener("click", clickCellHandler)
-});
 
-let resetBoard = (event) => {
-  for (var i = 1; i <= 9; i++) {
-    game[i] = 'not played';
-    document.getElementById(i).innerHTML = '';
-    document.getElementById(i).style.color = 'gray';
-  }
-
-  player = winner;
-  turn.style.color = 'gray'
-  turn.innerHTML = `Player ${player}, it's your turn!`
-  score.innerHTML = `X\t${tally.X}:${tally.O}\tO`
-}
-
-document.getElementById('reset').addEventListener("click", resetBoard);
-resetBoard();
