f96d7783e7bff381b134166e7581b0c343e8e30c
Robert Heler <robertheler@gmail.com>
2020-04-20 17:30:46 -0700

    All variables in one state object

--- challenge_1/app.js
+++ challenge_1/app.js
@@ -1,13 +1,14 @@
 // model
-let game, winner, player, playerOne, playerTwo;
-initalize();
+let state = initalize();
 
 function initalize () {
-  game = [];
-  winner = 'X';
-  player = 'X';
-  playerOne = {score: 0, name: prompt('Player one (X), please enter your name:', 'X')};
-  playerTwo = {score: 0, name: prompt('Player two (O), please enter your name:', 'O')};
+  let obj = {};
+  obj.game = [];
+  obj.winner = 'X';
+  obj.player = 'X';
+  obj.playerOne = {score: 0, name: prompt('Player one (X), please enter your name:', 'X') || 'X'};
+  obj.playerTwo = {score: 0, name: prompt('Player two (O), please enter your name:', 'O') || 'O'};
+  return obj
 }
 
 //view
@@ -19,7 +20,7 @@ resetBoard();
 document.querySelectorAll('td').forEach(tableCell => {
   tableCell.addEventListener("click", (event) => {
     let cell = event.target;
-    if (game[cell.id] === 'not played') {
+    if (state.game[cell.id] === 'not played') {
       playCell(cell);
       togglePlayer();
       checkForVictory();
@@ -31,37 +32,37 @@ document.getElementById('reset').addEventListener("click", resetBoard);
 
 function resetBoard (event) {
   for (var i = 1; i <= 9; i++) {
-    game[i] = 'not played';
+    state.game[i] = 'not played';
     document.getElementById(i).innerHTML = '';
     document.getElementById(i).style.color = 'gray';
   }
 
-  player = winner;
+  state.player = state.winner;
   turn.style.color = 'gray';
-  turn.innerHTML = winner === 'X' ? `${playerOne.name}, it's your turn!` : `${playerTwo.name}, it's your turn!`;
-  score.innerHTML = `${playerOne.name} ${playerOne.score}:${playerTwo.score} ${playerTwo.name}`;
+  turn.innerHTML = state.winner === 'X' ? `${state.playerOne.name}, it's your turn!` : `${state.playerTwo.name}, it's your turn!`;
+  score.innerHTML = `${state.playerOne.name}&nbsp&nbsp&nbsp&nbsp${state.playerOne.score}&nbsp:&nbsp${state.playerTwo.score}&nbsp&nbsp&nbsp&nbsp${state.playerTwo.name}`;
   document.getElementById('reset').blur();
 }
 
 
 togglePlayer = () => {
-  if (player === 'X') {
-    player = 'O';
-    turn.innerHTML = `${playerTwo.name}, it's your turn!`;
+  if (state.player === 'X') {
+    state.player = 'O';
+    turn.innerHTML = `${state.playerTwo.name}, it's your turn!`;
   } else {
-    player = 'X';
-    turn.innerHTML = `${playerOne.name}, it's your turn!`;
+    state.player = 'X';
+    turn.innerHTML = `${state.playerOne.name}, it's your turn!`;
   }
 }
 
 playCell = (cell) => {
-  if (player === 'O') {
-    game[cell.id] = 'O';
+  if (state.player === 'O') {
+    state.game[cell.id] = 'O';
     cell.style.color = 'black';
   } else {
-    game[cell.id] = 'X';
+    state.game[cell.id] = 'X';
   }
-  cell.innerHTML = player;
+  cell.innerHTML = state.player;
 }
 checkForVictory = () => {
   checkRows();
@@ -70,54 +71,50 @@ checkForVictory = () => {
 }
 
 checkRows = () => {
-  if (game[1] + game[2] + game[3] === 'XXX') declareVictory('X', 1, 2, 3);
-  if (game[4] + game[5] + game[6] === 'XXX') declareVictory('X', 4, 5, 6);
-  if (game[7] + game[8] + game[9] === 'XXX') declareVictory('X', 7, 8, 9);
-  if (game[1] + game[2] + game[3] === 'OOO') declareVictory('O', 1, 2, 3);
-  if (game[4] + game[5] + game[6] === 'OOO') declareVictory('O', 4, 5, 6);
-  if (game[7] + game[8] + game[9] === 'OOO') declareVictory('O', 7, 8, 9);
+  if (state.game[1] + state.game[2] + state.game[3] === 'XXX') declareVictory('X', 1, 2, 3);
+  if (state.game[4] + state.game[5] + state.game[6] === 'XXX') declareVictory('X', 4, 5, 6);
+  if (state.game[7] + state.game[8] + state.game[9] === 'XXX') declareVictory('X', 7, 8, 9);
+  if (state.game[1] + state.game[2] + state.game[3] === 'OOO') declareVictory('O', 1, 2, 3);
+  if (state.game[4] + state.game[5] + state.game[6] === 'OOO') declareVictory('O', 4, 5, 6);
+  if (state.game[7] + state.game[8] + state.game[9] === 'OOO') declareVictory('O', 7, 8, 9);
 }
 
 checkColumns = () => {
-  if (game[1] + game[4] + game[7] === 'XXX') declareVictory('X', 1, 4, 7);
-  if (game[2] + game[5] + game[8] === 'XXX') declareVictory('X', 2, 5, 8);
-  if (game[3] + game[6] + game[9] === 'XXX') declareVictory('X', 3, 6, 9);
-  if (game[1] + game[4] + game[7] === 'OOO') declareVictory('O', 1, 4, 7);
-  if (game[2] + game[5] + game[8] === 'OOO') declareVictory('O', 2, 5, 8);
-  if (game[3] + game[6] + game[9] === 'OOO') declareVictory('O', 3, 6, 9);
+  if (state.game[1] + state.game[4] + state.game[7] === 'XXX') declareVictory('X', 1, 4, 7);
+  if (state.game[2] + state.game[5] + state.game[8] === 'XXX') declareVictory('X', 2, 5, 8);
+  if (state.game[3] + state.game[6] + state.game[9] === 'XXX') declareVictory('X', 3, 6, 9);
+  if (state.game[1] + state.game[4] + state.game[7] === 'OOO') declareVictory('O', 1, 4, 7);
+  if (state.game[2] + state.game[5] + state.game[8] === 'OOO') declareVictory('O', 2, 5, 8);
+  if (state.game[3] + state.game[6] + state.game[9] === 'OOO') declareVictory('O', 3, 6, 9);
 }
 
 checkDiagonals = () => {
-  if (game[1] + game[5] + game[9] === 'XXX') declareVictory('X', 1, 5, 9);
-  if (game[3] + game[5] + game[7] === 'XXX') declareVictory('X', 3, 5, 7);
-  if (game[1] + game[5] + game[9] === 'OOO') declareVictory('O', 1, 5, 9);
-  if (game[3] + game[5] + game[7] === 'OOO') declareVictory('O', 3, 5, 7);
+  if (state.game[1] + state.game[5] + state.game[9] === 'XXX') declareVictory('X', 1, 5, 9);
+  if (state.game[3] + state.game[5] + state.game[7] === 'XXX') declareVictory('X', 3, 5, 7);
+  if (state.game[1] + state.game[5] + state.game[9] === 'OOO') declareVictory('O', 1, 5, 9);
+  if (state.game[3] + state.game[5] + state.game[7] === 'OOO') declareVictory('O', 3, 5, 7);
 }
 
 declareVictory = (player, cell1, cell2, cell3) => {
-  winner = player;
-  if(winner === 'X') {
-    turn.innerHTML = `${playerOne.name} won!`;
-    playerOne.score++;
-    //update score
+  state.winner = player;
+  if(state.winner === 'X') {
+    turn.innerHTML = `${state.playerOne.name} won!`;
+    state.playerOne.score++;
   } else {
-    turn.innerHTML = `${playerTwo.name} won!`;
-    playerTwo.score++;
+    turn.innerHTML = `${state.playerTwo.name} won!`;
+    state.playerTwo.score++;
   }
-  score.innerHTML = `${playerOne.name} ${playerOne.score}:${playerTwo.score} ${playerTwo.name}`;
-  //turn.innerHTML = player === 'X' ? `${playerOne.name} won!` : `${playerTwo.name} won!`
+  score.innerHTML = `${state.playerOne.name}&nbsp&nbsp&nbsp&nbsp${state.playerOne.score}&nbsp:&nbsp${state.playerTwo.score}&nbsp&nbsp&nbsp&nbsp${state.playerTwo.name}`;
 
-  turn.style.color = 'red';
-  document.getElementById(cell1).style.color = 'red';
-  document.getElementById(cell2).style.color = 'red';
-  document.getElementById(cell3).style.color = 'red';
+  turn.style.color = '#8b0000';
+  document.getElementById(cell1).style.color = '#8b0000';
+  document.getElementById(cell2).style.color = '#8b0000';
+  document.getElementById(cell3).style.color = '#8b0000';
 
 
   for (var i = 1; i <= 9; i++) {
-    game[i] = 'game ended';
+    state.game[i] = 'game ended';
   }
-
-  //winner === 'X' ? playerOne.score++ : playerTwo.score++;
 }
 
 
