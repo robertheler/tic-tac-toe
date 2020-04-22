fad497c85a1bec23c2cc54dcb01e64f4301f9e69
Robert Heler <robertheler@gmail.com>
2020-04-22 16:40:30 -0700

    front end completed, order submitted to the server

--- challenge_3/client/app.jsx
+++ challenge_3/client/app.jsx
@@ -1,6 +1,144 @@
 //console.log('App.js sourced');
 
+class Purchase extends React.Component {
+  constructor(props) {
+    super(props)
+    this.handleSubmit = this.handleSubmit.bind(this);
+    //console.log('F3 props', props);
+  }
 
+  handleSubmit(event) {
+    event.preventDefault();
+    console.log(this.props);
+    fetch('http://127.0.0.1:3000/order', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(this.props),
+    })
+    .then(() => {
+      window.alert('Ordered Submitted!');
+      ReactDOM.render(<StartCheckout />, document.getElementById('app'));
+    })
+    .catch((error) => {
+      window.alert('Error with your order', error);
+    })
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>Name: {this.props.user.name}</div>
+        <div>Email: {this.props.user.email}</div>
+        <div>Password: {this.props.user.passowrd}</div>
+        <div>Shipping Address Line 1: {this.props.address.line1}</div>
+        <div>Shipping Address Line 2: {this.props.address.line1}</div>
+        <div>City: {this.props.address.city}</div>
+        <div>State: {this.props.address.state}</div>
+        <div>Credit Card: {this.props.payment.card}</div>
+        <div>Expiration Date: {this.props.payment.card}</div>
+        <div>CVV: {this.props.payment.cvv}</div>
+        <div>Billing Zip Code: {this.props.payment.billing}</div>
+        <input type="submit" value="Confirm order!" />
+      </form>
+    );
+  }
+}
+
+class F3 extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {card: '', expiration: '', cvv: '', billing: ''}
+    this.handleSubmit = this.handleSubmit.bind(this);
+    //console.log('F3 props', props);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let card = document.getElementById('card').value;
+    let expiration = document.getElementById('expiration').value;
+    let cvv = document.getElementById('cvv').value;
+    let billing = document.getElementById('billing').value;
+
+    this.state = {card, expiration, cvv, billing};
+    //console.log('F3 state', this.state);
+    ReactDOM.render(<Purchase user={this.props.user} address={this.props.address} payment={this.state}/>, document.getElementById('app'));
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          Credit Card:
+          <input id ="card" type="text" defaultValue={this.state.card} />
+        </div>
+        <div>
+          Expiration Date:
+          <input id ="expiration" type="text" defaultValue={this.state.expiration} />
+        </div>
+        <div>
+          CVV:
+          <input id="cvv" type="text" defaultValue={this.state.cvv} />
+        </div>
+        <div>
+          Billing Zip Code:
+          <input id="billing" type="text" defaultValue={this.state.billing} />
+        </div>
+        <input type="submit" value="Review Order" />
+      </form>
+    );
+  }
+
+}
+class F2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {line1: '', line2: '', city: '', state: '', zipcode: ''};
+    this.handleSubmit = this.handleSubmit.bind(this);
+    //console.log('F2 props', props);
+  }
+
+  handleSubmit(event) {
+    event.preventDefault();
+    let line1 = document.getElementById('line1').value;
+    let line2 = document.getElementById('line2').value;
+    let city = document.getElementById('city').value;
+    let state = document.getElementById('state').value;
+    let zipcode = document.getElementById('zipcode').value;
+    this.state = {line1, line2, city, state, zipcode};
+    //console.log('F2 state', this.state);
+    ReactDOM.render(<F3 user={this.props.user} address={this.state}/>, document.getElementById('app'));
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <div>
+          Street address (line 1):
+          <input id ="line1" type="text" defaultValue={this.state.line1} />
+        </div>
+        <div>
+        Street address (line 2):
+          <input id ="line2" type="text" defaultValue={this.state.line2} />
+        </div>
+        <div>
+        City:
+          <input id="city" type="text" defaultValue={this.state.city} />
+        </div>
+        <div>
+        State:
+          <input id="state" type="text" defaultValue={this.state.state} />
+        </div>
+        <div>
+        Zip Code:
+          <input id="zipcode" type="text" defaultValue={this.state.zipcode} />
+        </div>
+        <input type="submit" value="Next" />
+      </form>
+    );
+  }
+}
 
 class F1 extends React.Component {
   constructor(props) {
@@ -11,10 +149,16 @@ class F1 extends React.Component {
   }
 
   handleSubmit(event) {
-    console.log(event.currentTarget);
-
     event.preventDefault();
-  }
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let password = document.getElementById('password').value;
+
+    this.state = {name, email, password};
+
+    ReactDOM.render(<F2 user={this.state}/>, document.getElementById('app'));
+
+    }
 
   render() {
     console.log("rendered");
@@ -32,16 +176,21 @@ class F1 extends React.Component {
           Password:
           <input id="password" type="password" defaultValue={this.state.password} />
         </div>
-        <input type="submit" value="Next" />
+        <input type="submit" value="Continue to Address" />
       </form>
     );
   }
 }
 
+let StartCheckout = () => (
+  <form onSubmit={continueToF1}>
+    <input type="submit" value="Start Checkout" />
+  </form>
+)
 
-let a = "message";
-var App = () => (<F1 />);
-
-
+let continueToF1 = (event) => {
+  event.preventDefault();
+  ReactDOM.render(<F1 />, document.getElementById('app'));
+}
 //typically in a seperate index.js file
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(<StartCheckout />, document.getElementById('app'));
--- challenge_3/server.js
+++ challenge_3/server.js
@@ -1,20 +1,28 @@
 const express = require ('express');
+const cors = require ('cors');
 const app = express();
 const PORT  = 3000;
 //if you access http://127.0.0.1:3000/ from chrome it will serve index.html in /public
 //without it, http://127.0.0.1:3000 would still send a GET request to the server but nothing will be rendered
 //overrides the "/" get method in server.js
 app.use(express.static('public'));
+app.use(cors());
+var bodyParser = require('body-parser');
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use( bodyParser.json() );
 
 app.listen(PORT, () => console.log(`Server is listening on http://127.0.0.1:${PORT}`));
 
+app.post('/order', (req, res) => {
+  console.log("POST/ request received");
+  console.log(req.body);
+  res.end();
+});
 
 app.get('/', (req, res) => {
   console.log("GET/ request received");
   res.end();
 });
 
-app.post('/', (req, res) => {
-  console.log("POST/ request received");
-  res.end();
-});
