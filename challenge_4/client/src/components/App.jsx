55def4b73326d5785817ee753c16eb388108e649
Robert Heler <robertheler@gmail.com>
2020-04-23 12:59:48 -0700

    CSS

--- challenge_4/client/src/components/App.jsx
+++ challenge_4/client/src/components/App.jsx
@@ -1,1 +1,33 @@
-export const App = () => (<h1>Hi!!!</h1>);
+export const App = () => (<div><Board /></div>);
+
+class Board extends React.Component {
+  constructor(props) {
+    super(props);
+    let row = new Array(7).fill(0);
+    let baord = new Array(6).fill(row);
+
+    this.state = {board: baord};
+
+    //this.handleSubmit = this.handleSubmit.bind(this);
+  }
+  render() {
+    var circles = [];
+    for (let i = 0; i < 6; i++) {
+      for (let j = 0; j < 7; j ++) {
+        circles.push(<Circle row={i} column={j} palyer={this.state.board[i][j]}/>);
+      }
+    }
+
+    return (<div className ="board">{circles}</div>)
+  }
+}
+
+class Circle extends React.Component {
+  constructor(props) {
+    super(props);
+    //this.state = {index: props.column};
+  }
+  render() {
+    return (<div className="circle" id={`_${this.props.row}${this.props.column}`}></div>)
+  }
+}
