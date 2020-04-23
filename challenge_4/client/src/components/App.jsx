d98133cdf3d074926625dc5870662436cbbaf619
Robert Heler <robertheler@gmail.com>
2020-04-23 15:08:01 -0700

    place pieces, toggle player and hover

--- challenge_4/client/src/components/App.jsx
+++ challenge_4/client/src/components/App.jsx
@@ -3,22 +3,61 @@ export const App = () => (<div><Board /></div>);
 class Board extends React.Component {
   constructor(props) {
     super(props);
-    let row = new Array(7).fill(0);
-    let baord = new Array(6).fill(row);
+    // red player = 1, yellow player = 2, 0 = empty
+    //let row = new Array(7).fill(0);
+    //let board = new Array(6).fill(row);
+    var board = [];
+    for(var i=0; i<6; i++) {
+      board[i] = [];
+        for(var j=0; j<7; j++) {
+          board[i][j] = 0;
+      }
+    }
+    board[5][3] = 1;
+    this.state = {board: board, player: 1};
+    this.handleClick = this.handleClick.bind(this);
 
-    this.state = {board: baord};
-
-    //this.handleSubmit = this.handleSubmit.bind(this);
   }
+  handleClick(row, column) {
+    this.placePiece(column);
+    //check victory
+  }
+
+  placePiece(column) {
+    for (let row = 5; row >= 0; row--) {
+      if(this.state.board[row][column] === 0) {
+        this.state.board[row][column] = this.state.player;
+        this.render();
+        console.log(`Player ${this.state.player} placed at position ${row}-${column}`);
+        console.log(this.state.board);
+        this.togglePlayer();
+        break;
+      }
+    }
+  }
+
+  togglePlayer() {
+    if (this.state.player === 1) {
+      this.setState({player: 2})
+      //this.state.player = 2;
+    } else {
+      this.setState({player: 1})
+    }
+  }
+
   render() {
     var circles = [];
     for (let i = 0; i < 6; i++) {
       for (let j = 0; j < 7; j ++) {
-        circles.push(<Circle row={i} column={j} palyer={this.state.board[i][j]}/>);
+        circles.push(<Circle
+          row={i}
+          column={j}
+          occupancy={this.state.board[i][j]}
+          onClick={this.handleClick}/>);
       }
     }
 
-    return (<div className ="board">{circles}</div>)
+    return (<div className="board">{circles}</div>)
   }
 }
 
@@ -26,8 +65,27 @@ class Circle extends React.Component {
   constructor(props) {
     super(props);
     //this.state = {index: props.column};
+    this.handleClick = this.handleClick.bind(this);
+    this.handleHover = this.handleHover.bind(this);
+  }
+
+  handleHover(event){
+    console.log(event.currentTarget.getAttribute('column'));
+  }
+  handleClick(event) {
+    let row = event.currentTarget.getAttribute('row');
+    let column = event.currentTarget.getAttribute('column');
+    this.props.onClick(row, column)
   }
   render() {
-    return (<div className="circle" id={`_${this.props.row}${this.props.column}`}></div>)
+    switch (this.props.occupancy) {
+      case 1:
+        return (<div className="redCircle" onClick={this.handleClick} onMouseEnter={this.handleHover} row={this.props.row} column ={this.props.column}></div>);
+      case 2:
+        return (<div className="yellowCircle" onClick={this.handleClick} onMouseEnter={this.handleHover} row={this.props.row} column ={this.props.column}></div>)
+      default:
+        return (<div className="emptyCircle" onClick={this.handleClick} onMouseEnter={this.handleHover} row={this.props.row} column ={this.props.column}></div>)
+    }
+
   }
 }
